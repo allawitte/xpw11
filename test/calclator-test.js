423d13802da69fb509ce333c3aae7e4dadc681db
Alla Klimova <alla_88@inbox.lv>
2017-02-13 11:45:24 -0300

    test refactored - createCalculator

--- test/calclator-test.js
+++ test/calclator-test.js
@@ -1,11 +1,13 @@
 'use strict';
 var assert = require('chai').assert;
 const Calculator = require('../src/clculator');
-
+var createCalculator = function(){
+    return new Calculator();
+}
 suite('When calculator gets', function(){
     suite(' empty input', function () {
         test('- returns 0', function (){
-            const calculator = new Calculator();
+            const calculator = createCalculator();
             let sum = calculator.add();
             assert.equal(sum, 0);
         });
@@ -13,7 +15,7 @@ suite('When calculator gets', function(){
 
     suite(' 1', function () {
         test('- returns 1', function (){
-            const calculator = new Calculator();
+            const calculator = createCalculator();
             let sum = calculator.add(1);
             assert.equal(sum, 1);
         });
