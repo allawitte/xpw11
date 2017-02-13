fffaa0ecba4b9c7e112de27ea51bd938df8c6730
Alla Klimova <alla_88@inbox.lv>
2017-02-13 11:13:29 -0300

    empty value

---
+++ test/calclator-test.js
@@ -0,0 +1,19 @@
+'use strict';
+var assert = require('chai').assert;
+class Calculator {
+    add(){
+        return 0;
+    }
+}
+suite('When calculator gets empty input', function () {
+    test('- returns 0', function (){
+        const calculator = new Calculator();
+        let sum = calculator.add();
+        assert.equal(sum, 0);
+    });
+});
+
+
+/**
+ * Created by HP on 2/13/2017.
+ */
