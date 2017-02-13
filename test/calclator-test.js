985c19004a70dc86e1ef357b34c74f2cc9a56f45
Alla Klimova <alla_88@inbox.lv>
2017-02-13 11:27:09 -0300

    test for 1 added, falls

--- test/calclator-test.js
+++ test/calclator-test.js
@@ -1,13 +1,26 @@
 'use strict';
 var assert = require('chai').assert;
 const Calculator = require('../src/clculator');
-suite('When calculator gets empty input', function () {
-    test('- returns 0', function (){
-        const calculator = new Calculator();
-        let sum = calculator.add();
-        assert.equal(sum, 0);
+suite('When calculator gets', function(){
+    suite(' empty input', function () {
+        test('- returns 0', function (){
+            const calculator = new Calculator();
+            let sum = calculator.add();
+            assert.equal(sum, 0);
+        });
+    });
+
+    suite(' 1', function () {
+        test('- returns 1', function (){
+            const calculator = new Calculator();
+            let sum = calculator.add();
+            assert.equal(sum, 1);
+        });
     });
 });
+
+
+
 
 
 /**
